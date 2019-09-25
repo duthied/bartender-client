de04574a6316068711f74e2e7fa6e17ff45e7122
Devlon Duthie <duthied@gmail.com>
2019-09-25 15:23:16 -0400

    Initial cut with most/all pages and components stubbed out

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+// import { Router } from '@reach/router';
+
+// import Launch from './launch';
+// import Launches from './launches';
+// import Cart from './cart';
+// import Profile from './profile';
+// import { Footer, PageContainer } from '../components';
+
+export default function Pages() {
+  return (
+    <Fragment>
+      {/* <PageContainer>
+          navigation below
+        <Router primary={false} component={Fragment}>
+          <Launches path="/" />
+          <Launch path="launch/:launchId" />
+          <Cart path="cart" />
+          <Profile path="profile" />
+        </Router>
+      </PageContainer>
+      <Footer /> */}
+    </Fragment>
+  );
+}
