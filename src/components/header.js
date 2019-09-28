94a3df9d693befb0aa13b61182f799307853f92d
Devlon Duthie <duthied@gmail.com>
2019-09-27 21:15:41 -0400

    Progress on main page

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'react-emotion';
+
+import { unit } from '../styles';
+
+const siteTitle = 'Bartender';
+
+export default function Header(props) {
+  return (
+    <Container>
+      <div>
+        <h2>{siteTitle}</h2>
+      </div>
+    </Container>
+  );
+  }
+  
+  /**
+   * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
+   */
+  
+  const Container = styled('div')({
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: unit * 4.5,
+    marginLeft: unit * 1,
+  });
+  
